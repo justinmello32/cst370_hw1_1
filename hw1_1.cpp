829dc5e01783ac0a243c9f12e89ac2db61de8492
Justin Mello <mello23@llnl.gov>
2021-03-05 11:21:41 -0800

    Close to working copy, just need to debug.

--- hw1_1.cpp
+++ hw1_1.cpp
@@ -1,27 +1,52 @@
 #include <iostream>
-
+#include <algorithm>
 
 using namespace std;
 int main() {
 
     int inputAmount = 0;
 
-    //Get User input for size of numbers
+    //Get User input for size of numbers and define
+    //additional variables for processing.
     cin >> inputAmount;
     int input [inputAmount];
+    int mostFrequentNumber = input[0];
+    int tempMostFrequentNumber = 0;
+    int numFrequency= 1;
+    int tempNumFrequency = 0;
 
     //Loop through array to build array
     for(int i = 0; i < inputAmount; i++){
         cin >> input[i];
     }
 
+    //Sort Array
+    sort(input, input + inputAmount);
+
     //Check to find most common number
     for(int i = 0; i < inputAmount; i++) {
-        cout << input[i];
-    }
+        cout << input[i] << " ";
 
-    cout << "Number: " << endl;
-    cout << "Frequency: " << endl;
+        if(input[i] == input[i - 1]) {
+            tempNumFrequency++;
+        }
+        else {
+            if(tempNumFrequency > numFrequency) {
+                numFrequency = tempNumFrequency;
+                mostFrequentNumber = input[i - 1];
+            }
+            tempNumFrequency = 1;
+        }
+
+        if(tempNumFrequency > numFrequency) {
+            numFrequency = tempNumFrequency;
+            mostFrequentNumber = input[i - 1];
+        }
+
+    }
+    cout << " " << endl;
+    cout << "Number: " << mostFrequentNumber << endl;
+    cout << "Frequency: " << numFrequency << endl;
     return 0;
 
 }
